cd18e92f45585bafeaa040a1f11861aff585eef3
Fiona-Yang-IMM <65745820+Fiona-Yang-IMM@users.noreply.github.com>
2020-07-21 17:53:29 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -52,8 +52,8 @@
                     <h2>Hydroenergy</h2>
                     <div class="content">
                         <img class="img-t" src="img/hydroenergy.jpg" alt="hydroenergy">
-                        <p>Lorem ipsum dolor sit amett enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        onsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <p>Lorem ipsum dolor sit amett enim ad minim veniam, quis nostrud exercitation ullamco . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        .Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
                     <div>   
                 </figure>
